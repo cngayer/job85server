aa2a432ac6a069418d5047af4a65e81447e8a897
cngayer <107828547+cngayer@users.noreply.github.com>
2022-06-20 13:58:14 +0800

    zhuanzhuan init

---
+++ src/cartservice/src/cartstore/RedisCartStore.cs
@@ -0,0 +1,118 @@
+// 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Caching.Distributed;
+using job85.Protobuf;
+
+namespace cartservice.cartstore
+{
+    public class RedisCartStore : ICartStore
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisCartStore(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task AddItemAsync(string userId, string productId, int quantity)
+        {
+            Console.WriteLine($"AddItemAsync called with userId={userId}, productId={productId}, quantity={quantity}");
+
+            try
+            {
+                job85.Cart cart;
+                var value = await _cache.GetAsync(userId);
+                if (value == null)
+                {
+                    cart = new job85.Cart();
+                    cart.UserId = userId;
+                    cart.Items.Add(new job85.CartItem { ProductId = productId, Quantity = quantity });
+                }
+                else
+                {
+                    cart = job85.Cart.Parser.ParseFrom(value);
+                    var existingItem = cart.Items.SingleOrDefault(i => i.ProductId == productId);
+                    if (existingItem == null)
+                    {
+                        cart.Items.Add(new job85.CartItem { ProductId = productId, Quantity = quantity });
+                    }
+                    else
+                    {
+                        existingItem.Quantity += quantity;
+                    }
+                }
+                await _cache.SetAsync(userId, cart.ToByteArray());
+            }
+            catch (Exception ex)
+            {
+                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex}"));
+            }
+        }
+
+        public async Task EmptyCartAsync(string userId)
+        {
+            Console.WriteLine($"EmptyCartAsync called with userId={userId}");
+
+            try
+            {
+                var cart = new job85.Cart();
+                await _cache.SetAsync(userId, cart.ToByteArray());
+            }
+            catch (Exception ex)
+            {
+                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex}"));
+            }
+        }
+
+        public async Task<job85.Cart> GetCartAsync(string userId)
+        {
+            Console.WriteLine($"GetCartAsync called with userId={userId}");
+
+            try
+            {
+                // Access the cart from the cache
+                var value = await _cache.GetAsync(userId);
+
+                if (value != null)
+                {
+                    return job85.Cart.Parser.ParseFrom(value);
+                }
+
+                // We decided to return empty cart in cases when user wasn't in the cache before
+                return new job85.Cart();
+            }
+            catch (Exception ex)
+            {
+                throw new RpcException(new Status(StatusCode.FailedPrecondition, $"Can't access cart storage. {ex}"));
+            }
+        }
+
+        public bool Ping()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
